2844800660e25a59e200db53613bf37da231cd68
unlomtrois <vadim.kuz02@gmail.com>
2026-04-11 12:38:38 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,16 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [
+  packages = with pkgs; [
     go_1_25
     golangci-lint
     gopls
     gnumake
   ];
-
-  shellHook = ''
-    export GO111MODULE=on
-    export GOPATH=$(pwd)
-    export PATH=$(pwd)/bin:$PATH
-  '';
 }
